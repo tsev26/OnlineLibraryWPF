0e39955091fdd389360729f6320b914caca33e44
TSev26 <tsevcu@gmail.com>
2022-12-13 15:36:12 +0100

    1

--- OnlineLibraryWPF/ViewModels/RentalsViewModel.cs
+++ OnlineLibraryWPF/ViewModels/RentalsViewModel.cs
@@ -1,12 +1,78 @@
-﻿using System;
+﻿using OnlineLibraryWPF.MongoDB;
+using OnlineLibraryWPF.Services;
+using OnlineLibraryWPF.Stores;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace OnlineLibraryWPF.ViewModels
 {
     public class RentalsViewModel : ViewModelBase
     {
+        public ICommand NavigateLibrarianMenuCommand { get; }
+        public ICommand NavigateCustomerMenuCommand { get; }
+        public ICommand RentBookCommand { get; }
+        public ICommand LoadRentalsCommand { get; }
+
+        public bool IsLoading { get; set; }
+        public UserStore UserStore { get; set; }
+        public MessageStore MessageStore { get; set; }
+
+        public bool IsBookSelected => UserStore.Book != null;
+
+        public RentalsViewModel(UserStore userStore,
+                              UsersService usersService,
+                              MessageStore messageStore,
+                              INavigationService navigateLibrarianCommand,
+                              INavigationService navigateCustomerMenuCommand)
+        {
+            UserStore = userStore;
+            MessageStore = messageStore;
+
+            MessageStore.MessageChanged += MessageStore_MessageChanged;
+            UserStore.BookChanged += UserStore_BookChanged;
+        }
+
+        private void UserStore_BookChanged()
+        {
+            OnPropertyChanged(nameof(IsBookSelected));
+        }
+
+        public override void Dispose()
+        {
+            MessageStore.MessageChanged -= MessageStore_MessageChanged;
+            UserStore.BookChanged -= UserStore_BookChanged;
+            base.Dispose();
+        }
+
+        private void MessageStore_MessageChanged()
+        {
+            OnPropertyChanged(nameof(MessageStore));
+        }
+
+        public static RentalsViewModel LoadViewModel(UserStore userStore,
+                                                     UsersService usersService,
+                                                     MessageStore messageStore,
+                                                     INavigationService navigateLibrarianCommand,
+                                                     INavigationService navigateCustomerMenuCommand)
+        {
+            RentalsViewModel viewModel = new RentalsViewModel(userStore,
+                                                          usersService,
+                                                          messageStore,
+                                                          navigateLibrarianCommand,
+                                                          navigateCustomerMenuCommand);
+
+            viewModel.Books = new ObservableCollection<Book>();
+
+            viewModel.LoadBooksCommand.Execute(null);
+
+            return viewModel;
+        }
+
+
+        public ObservableCollection<Book> Books { get; set; }
     }
 }
